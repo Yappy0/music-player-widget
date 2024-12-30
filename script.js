0d1b774e244f6476a9dc7dbbbe24768183e33619
Cayden Yap <83028586+Yappy0@users.noreply.github.com>
2024-12-30 23:00:46 +0000

    playPause II

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ file = urlParams.get("file");
 var playing = false;
 function playPause(e){
     playing = !playing;
-    alert(playing);
+    let playPauseBtn = document.getElementById("playPauseBtn");
+    if (playing){
+        playPauseBtn.classList.remove("bi-play-fill");
+        playPauseBtn.classList.add("bi-pause-fill");
+    } else {
+        playPauseBtn.classList.remove("bi-pause-fill");
+        playPauseBtn.classList.add("bi-play-fill");
+    }
 }
 
 document.getElementById("playPauseBtn").addEventListener("click", playPause);
