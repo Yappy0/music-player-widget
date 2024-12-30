20c0be522c7d54287fee8861f5cd547fee611735
Cayden Yap <83028586+Yappy0@users.noreply.github.com>
2024-12-30 23:04:05 +0000

    playPause III: added audio

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const urlParams = new URLSearchParams(window.location.search);
 file = urlParams.get("file");
 
+a = new Audio();
+a.src = file;
 var playing = false;
+
 function playPause(e){
     playing = !playing;
     let playPauseBtn = document.getElementById("playPauseBtn");
     if (playing){
         playPauseBtn.classList.remove("bi-play-fill");
         playPauseBtn.classList.add("bi-pause-fill");
+        a.play();
     } else {
         playPauseBtn.classList.remove("bi-pause-fill");
         playPauseBtn.classList.add("bi-play-fill");
+        a.pause();
     }
 }
 
