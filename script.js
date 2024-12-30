57e6a2f8190f0d9591eac1a07d8d86887ffb0819
Cayden Yap <83028586+Yappy0@users.noreply.github.com>
2024-12-30 22:56:24 +0000

    playPause I

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const urlParams = new URLSearchParams(window.location.search);
 file = urlParams.get("file");
-alert(file);
+
+var playing = false;
+function playPause(e){
+    playing = !playing;
+    alert(playing);
+}
+
+document.getElementById("playPauseBtn").addEventListener("click", playPause);
